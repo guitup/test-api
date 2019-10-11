8e9c5c990c432f00ec56c834d2f18eec5e418a33
guitup <56364208+guitup@users.noreply.github.com>
2019-10-11 02:07:03 +0200

    api

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'vendor/mashape/unirest-php/src/Unirest.php';
+
+Unirest\Request::verifyPeer(false); 
+$response = Unirest\Request::get("https://api-football-v1.p.rapidapi.com/v2/fixtures/live?timezone=Europe%2FLondon",
+  array(
+    "X-RapidAPI-Host" => "api-football-v1.p.rapidapi.com",
+    "X-RapidAPI-Key" => "b416485ec8mshfd71789b346e56cp132b51jsn478ccb84d722"
+  )
+);
+
+
+$resultat = json_decode(json_encode($response),true);
+/*print_r($resultat);*/
+?>
+<table>
+    <tr>
+        <td>Heure du match</td>
+        <td>Equipe domicile</td>
+        <td>Equipe Exterieur</td>
+        <td>Temps de Jeu</td>
+        <td>Score</td>
+    </tr>
+<?php
+foreach ($response->body->api->fixtures as $match){
+    echo '<tr>';
+    echo '<td>'.date('d/m/Y', $match->event_timestamp).' à '. date('H:i:s', $match->event_timestamp).'</td>';
+    echo '<td>'.$match->homeTeam->team_name.'</td>';
+    echo '<td>'.$match->awayTeam->team_name.'</td>';
+    echo '<td>'.$match->elapsed.'</td>';
+    echo '<td>'.$match->goalsHomeTeam.' - '.$match->goalsAwayTeam.'</td>';
+    echo '</tr>';
+}
+
+?>
+</table>
